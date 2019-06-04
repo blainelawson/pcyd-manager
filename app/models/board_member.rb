22160711ea881272594f6349712b50b12965d7f1
Blaine Lawson <blaine.lawson@gmail.com>
2019-06-04 16:42:37 +0000

    migration attempts

--- app/models/board_member.rb
+++ app/models/board_member.rb
@@ -1,5 +1,4 @@
 class BoardMember < ActiveRecord::Base
-  has_many :committee_members
-  has_many :members, through: :committee_members
   has_many :committees
+  has_many :members, through: :committees
 end
--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,4 +1,5 @@
 class Committee < ActiveRecord::Base
   has_many :members
+  has_many :committee_members
   belongs_to :board_member
 end

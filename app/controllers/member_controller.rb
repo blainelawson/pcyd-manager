4402872251f96f3fce2f73411b74334161cbd226
Blaine Lawson <blaine.lawson@gmail.com>
2019-06-06 19:44:08 +0000

    Added new member functionality to controller and view:

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -6,6 +6,10 @@ class MemberController < ApplicationController
       erb :'/members/show_members'
   end
 
+  get '/members/new' do
+    erb :'/members/create_member'
+  end
+
   get '/members/:slug' do
     @member = Member.find_by_slug(params[:slug])
     erb :'members/show_member'
@@ -14,6 +18,11 @@ class MemberController < ApplicationController
   get '/members/:slug/edit' do
     @member = Member.find_by_slug(params[:slug])
     erb :'/members/edit_member'
+  end
+
+  post '/members' do
+    member = Member.create(params[:member])
+    redirect "/members/#{member.slug}"
   end
 
   patch '/members/:slug' do
